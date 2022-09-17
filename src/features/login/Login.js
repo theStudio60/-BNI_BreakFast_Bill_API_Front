da049538736a0686a41611f898f9d773c6140ce8
Julien <82111329+JulienOxi@users.noreply.github.com>
2022-09-17 09:54:06 -0500

    Login et logout fonctionnel

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -3,19 +3,19 @@ import { Formik } from "formik";
 import { Navigate } from 'react-router-dom';
 import apiBni from "../../conf/axios/api.bni";
 import cookies from 'js-cookie';
-
+import {Loading} from '../../components/utils';
+import { $CombinedState } from "redux";
 
 export default class Login extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { errorMessage: null };
+    this.state = { errorMessage: null, loaded: false, auth:false };
   }
   
-
   //validation du login
   submit = (values, actions) => {
-    this.setState({errorMessage:null})
+    this.setState({errorMessage:null, loaded:true})
     apiBni
       .post("/login", values, {
         headers: {
@@ -25,17 +25,17 @@ export default class Login extends Component {
       .then((response) => {
         if (response.status === 200) {
           cookies.set('BEARER', response.data.token, { sameSite:'strict' });
-          //redirection
+          this.setState({auth:true})
         }
       })
       //si login pas valide on update le state pour mettre un message d'erreur
       .catch((err) => {
-        this.setState({ errorMessage: err.response.data.message });
+        this.setState({ errorMessage: err.response.data.message, loaded:false });
         actions.isSubmitting = false;
         actions.resetForm();
       }
       );
-  };
+  };  
 
   //validation des données
   validate = (values) => {
@@ -47,19 +47,22 @@ export default class Login extends Component {
   };
 
   render() {
+    //si authentifié, on redirige vers /
+    if(this.state.auth){
+      return <Navigate to="/" />
+    }
+    //sinon on affiche le formulaire
     return (
-      <div
-        className="container-fluid p-5
-d-flex flex-column justify-content-center align-items-center"
-      >
+      <>
+      { this.state.loaded ? <Loading />: (
+      <div className="container-fluid p-5 d-flex flex-column justify-content-center align-items-center">
         {/* affichage du message d'erreur */}
         { 
         this.state.errorMessage &&
           <div className="alert alert-danger" role="alert">
             { this.state.errorMessage }
           </div>
-
-  }
+        }
 
         <Formik
           onSubmit={this.submit}
@@ -115,6 +118,8 @@ d-flex flex-column justify-content-center align-items-center"
           )}
         </Formik>
       </div>
+      )}
+      </>
     );
   }
 }
