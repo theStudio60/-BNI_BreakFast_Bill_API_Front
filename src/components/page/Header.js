83c2326ef3e517dccbb60159f9e4f0c4c529c364
toto <riedotoine@gmail.com>
2022-10-06 11:44:32 +0200

    header

--- src/components/page/Header.js
+++ src/components/page/Header.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import cookies from 'js-cookie';
 import {GrLogout, GrUser, GrSettingsOption } from 'react-icons/gr'
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
 
@@ -15,6 +16,22 @@ export default function Header() {
         return(
             <div className="header">
                 <p className='header--logo'>LOGO</p>
+                <div className='header--container'>
+                    <select name="Mois" className='header--container--select'>
+                        <option className='header--container--options' value="">Mois en cours</option>
+                    </select>
+                    <select name="Mois" className='header--container--select--small'>
+                        <option value="">2022</option>
+                    </select>
+                    <NavLink to="/rappel" className="header--container--link">
+                        
+                        Rappel{" "}
+                    </NavLink>
+                    <NavLink to="/archives" className="header--container--link">
+                        
+                        Archives{" "}
+                    </NavLink>
+                </div>
 
                 <div className="header--button--menu">
                     <button  className="header--button--icons"><GrUser className="header--buttons-icons-gr"/></button>
